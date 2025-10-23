ce0d1e8d090ed054d9b0cde4797b38169410dd86
Vahe Borisov <vaheborisovofficial@gmail.com>
2025-10-23 16:51:16 +0400

    create some data related to schema types and upload into sanity backend

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import {journalEntry} from './journalEntry'
+import {dailyPrompt} from './dailyPrompt'
+import {category} from './category'
+import {aiConversation} from './aiConversation'
+
+export const schemaTypes = [journalEntry, dailyPrompt, category, aiConversation]
